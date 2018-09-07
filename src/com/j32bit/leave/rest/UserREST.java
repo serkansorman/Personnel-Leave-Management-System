461cb3c26fd6a202c0fb9d67167c0b4009e8cfc4
serkan <serkan@serkan-X555LD>
2018-09-07 15:26:17 +0300

    UserREST oluşturuldu authenticate edilmiş userın alındığı metod yazıldı.

---
+++ src/com/j32bit/leave/rest/UserREST.java
@@ -0,0 +1,32 @@
+package com.j32bit.leave.rest;
+
+import javax.annotation.security.PermitAll;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import com.j32bit.leave.bean.User;
+
+@Path("/session")
+public class UserREST {
+
+
+	@Path("/getAuthenticatedUser")
+	@POST
+	@PermitAll
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getAuthenticatedUser(@Context HttpServletRequest request) {
+		
+		System.out.println("Entered rest");
+
+		HttpSession session = request.getSession();
+		User authenticatedUser = (User) session.getAttribute("LOGIN_USER");
+
+	
+		return authenticatedUser;
+	}
+}
