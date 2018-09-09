1d1228f0e721326fdaa6e739055248648af42fc4
serkan <serkan@serkan-X555LD>
2018-09-09 14:39:08 +0300

    Leave ve LeaveDAO güncellendi.
    addLeave metodu executeQuery execute olarak değiştirildi.

--- src/com/j32bit/leave/dao/LeaveDAO.java
+++ src/com/j32bit/leave/dao/LeaveDAO.java
@@ -2,7 +2,7 @@ package com.j32bit.leave.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
+import java.util.Properties;
 
 import com.j32bit.leave.bean.Leave;
 
@@ -12,15 +12,21 @@ public class LeaveDAO extends ConnectionHelper{
 		
 		Connection conn = getConnection();
 		PreparedStatement preparedStmt = conn.prepareStatement("INSERT INTO leaves (email,begin_date,end_date,status) VALUES (?,?,?,?)");
+		
 		preparedStmt.setString(1, leave.getOwner().getEmail());
 		preparedStmt.setString(2, leave.getBeginDate());
 		preparedStmt.setString(3, leave.getEndDate());
 		preparedStmt.setString(4, leave.getStatus());
-
-		preparedStmt.executeQuery();
+		
+		preparedStmt.execute();
+		
 		closePreparedStatement(preparedStmt);
 		closeConnection(conn);
 			
+	}
+	
+	public void start(Properties prop) throws Exception {
+		super.init(prop);
 	}
 
 }
