457daeda3d04845ae9c699281834ebd7b3c71bc6
serkan <serkan@serkan-X555LD>
2018-09-14 01:05:12 +0300

    İzin isteklerinin proje yöneticisi tarafından görüntülenmesi sağlandı.
    UserDAO ve UserRest için gerekli metod yazıldı. Sayfada JQuery ile
    izinlerin gösterimi sağlandı. İstek kabul ve red buton taslağı
    oluşturuldu.

--- src/com/j32bit/leave/dao/LeaveDAO.java
+++ src/com/j32bit/leave/dao/LeaveDAO.java
@@ -2,9 +2,12 @@ package com.j32bit.leave.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import com.j32bit.leave.bean.Leave;
+import com.j32bit.leave.bean.User;
 
 public class LeaveDAO extends ConnectionHelper{
 	
@@ -24,6 +27,40 @@ public class LeaveDAO extends ConnectionHelper{
 		closeConnection(conn);
 			
 	}
+	
+	public ArrayList<Leave> getLeaveRequests(String projectManager) throws Exception{
+		
+		Connection conn = getConnection();
+		PreparedStatement preparedStmt = conn.prepareStatement("SELECT * FROM users INNER JOIN leaves ON users.email = leaves.email AND projectManager=?");
+		preparedStmt.setString(1, projectManager);
+		ResultSet rst = preparedStmt.executeQuery();
+	    ArrayList<Leave> leaveList = new ArrayList<>();
+	    while (rst.next()) {
+	    	Leave leave = new Leave();
+	    	User user = new User();
+	    	
+	    	user.setEmail(rst.getString("email"));
+	    	user.setName(rst.getString("full_name"));
+	    	user.setTotalLeaveDays(rst.getInt("totalLeaveDays"));
+	    	
+	    	leave.setOwner(user);
+	    	leave.setBeginDate(rst.getString("begin_date"));
+	    	leave.setEndDate(rst.getString("end_date"));
+	    	leave.setStatus(rst.getString("status"));
+	    	leave.setId(rst.getLong("id"));
+
+	        leaveList.add(leave);
+	    }
+	    
+	    closeResultSet(rst);
+		closePreparedStatement(preparedStmt);
+		closeConnection(conn);
+		
+		return leaveList;
+		
+	}
+
+
 	
 	public void start(Properties prop) throws Exception {
 		super.init(prop);
--- src/com/j32bit/leave/rest/LeaveRest.java
+++ src/com/j32bit/leave/rest/LeaveRest.java
@@ -1,9 +1,14 @@
 package com.j32bit.leave.rest;
 
+import java.util.ArrayList;
+
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.j32bit.leave.bean.Leave;
@@ -19,5 +24,19 @@ public class LeaveRest {
 	public void addLeave(Leave leave) throws Exception {
 		ServiceFacade.getInstance().getLeaveDAO().addLeave(leave);
 	}
+	
+	
+	@Path("/getLeaveRequests")
+	@POST
+	@RolesAllowed("projectManager")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArrayList<Leave> getLeaveRequests(String projectManager) throws Exception{
+		System.out.println("Entered getLeaveRequests rest");
+		return ServiceFacade.getInstance().getLeaveDAO().getLeaveRequests(projectManager);
+	}
+	
+	
+
 
 }
